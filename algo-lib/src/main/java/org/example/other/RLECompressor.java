6c4e34d79eab58e7e9f9c18c9b0c9ed80a1f829a
Roman Koryakov <r.koryakov@russianpost.ru>
2026-04-07 14:31:35 +0300

    RLE

--- algo-lib/src/main/java/org/example/other/RLECompressor.java
+++ algo-lib/src/main/java/org/example/other/RLECompressor.java
@@ -1,4 +1,5 @@
 package org.example.other;
+import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.EOFException;
@@ -8,6 +9,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PushbackInputStream;
+import java.util.Arrays;
 
 /**
  *  RLE 
@@ -39,6 +41,107 @@ public class RLECompressor {
              FileOutputStream fos = new FileOutputStream(outputFile)) {
             
             decompress(fis, fos);
+        }
+    }
+    
+    /**
+     * Упакованный RLE с оптимизацией для неповторяющихся данных
+     * Формат: 
+     * - Флаг 0x00 + байт + счетчик для повторений
+     * - Флаг 0xFF + длина + данные для неповторяющихся блоков
+     */
+    public static void compressPacked(InputStream input, OutputStream output) 
+            throws IOException {
+        
+        PushbackInputStream pbInput = new PushbackInputStream(input, 1024);
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        
+        while (true) {
+            int currentByte = pbInput.read();
+            if (currentByte == -1) break;
+            
+            // Проверяем, начинается ли последовательность повторений
+            int nextByte = pbInput.read();
+            if (nextByte != -1 && nextByte == currentByte) {
+                // Нашли повторяющуюся последовательность
+                int count = 2;
+                
+                while (count < 255 && (nextByte = pbInput.read()) != -1 && nextByte == currentByte) {
+                    count++;
+                }
+                
+                if (nextByte != -1 && nextByte != currentByte) {
+                    pbInput.unread(nextByte);
+                }
+                
+                // Записываем маркер повторяющегося блока
+                output.write(0x00);
+                output.write(currentByte);
+                output.write(count);
+            } else {
+                // Собираем блок неповторяющихся данных
+                if (nextByte != -1) {
+                    pbInput.unread(nextByte);
+                }
+                
+                buffer.reset();
+                buffer.write(currentByte);
+                
+                while ((nextByte = pbInput.read()) != -1) {
+                    // Проверяем, не начинается ли новая последовательность
+                    int futureByte = pbInput.read();
+                    if (futureByte != -1 && futureByte == nextByte) {
+                        pbInput.unread(futureByte);
+                        break;
+                    }
+                    
+                    buffer.write(nextByte);
+                    
+                    if (buffer.size() >= 255) break;
+                    
+                    if (futureByte != -1) {
+                        pbInput.unread(futureByte);
+                    }
+                }
+                
+                // Записываем маркер неповторяющегося блока
+                output.write(0xFF);
+                output.write(buffer.size());
+                buffer.writeTo(output);
+            }
+        }
+    }
+    
+    /**
+     * Декомпрессия упакованного RLE
+     */
+    public static void decompressPacked(InputStream input, OutputStream output) 
+            throws IOException {
+        
+        while (true) {
+            int flag = input.read();
+            if (flag == -1) break;
+            
+            if (flag == 0x00) {
+                // Повторяющийся блок
+                int value = input.read();
+                int count = input.read();
+                
+                byte[] bytes = new byte[count];
+                Arrays.fill(bytes, (byte) value);
+                output.write(bytes);
+            } else if (flag == 0xFF) {
+                // Неповторяющийся блок
+                int length = input.read();
+                byte[] data = new byte[length];
+                int read = input.read(data);
+                if (read != length) {
+                    throw new IOException("Недостаточно данных для распаковки");
+                }
+                output.write(data);
+            } else {
+                throw new IOException("Неверный формат сжатых данных");
+            }
         }
     }
     
@@ -98,4 +201,5 @@ public class RLECompressor {
         
         output.flush();
     }
+    
 }
